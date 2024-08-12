9fa8afdd1ae4acee3e7a77e9e00c842c16b1e1ee
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-12 14:28:02 +0200

    using reducer hook

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useReducer, useState } from "react";
+
+function Counter() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state > 0 ? state - 1 : 0;
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div>
+      <p>Count: {state}</p>
+      <button onClick={()=>dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={()=>dispatch({ type: "decrement" })}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
